2f2894eaa4fe1f08df1a883815c7dc3d0246c902
Javier Huerta Jr <javsteratii@gmail.com>
2019-10-02 01:58:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,2 @@
-numbers = [1,2,3]
-
-def square_array(numbers)
-  new_numbers = []
-  numbers.each do |x|
-    new_numbers << x**2
-  end 
-  new_numbers
-end
+def square_array(list)
+  list.map |num| num**2
